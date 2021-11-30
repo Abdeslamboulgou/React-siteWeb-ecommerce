d4a17ceda398b3400a775cf3e2a0b5b2a2e9de14
Abdeslamboulgou <boulgouslam@gmail.com>
2021-11-30 14:49:47 +0100

    add nav bar elements and style them

--- src/pages/Nav.js
+++ src/pages/Nav.js
@@ -4,32 +4,31 @@ import '../style/pages/nav.css';
 
 
 function Nav() {
-    const {cartItems , showHideCart} = useContext(CartContenu)
+    const { cartItems, showHideCart } = useContext(CartContenu)
     return (
-       <nav className="nav_bar">
-        <div className="nav_tazlmat">Store</div>
-        {/* <div className="home">Home</div>
-        <div className="about">About</div>
-        <div className="products">Products</div>
-        <div className="contact">Contact</div>
-        <div className="blog">Blog</div>
-        */}
-        <div className="nav_amass">
-            <div className="input_wrapper" >
-                  <input placeholder="search..." type="text"></input>
-                   <i className="fas fa-search"/>
+        <nav >
+            {/*<div className="nav_tazlmat">Store</div>*/}
+            <div className="nav_tazlmat">
+                <div className="home">Home</div>
+                <div className="contact">Contact</div>
+                <div className="products">Products</div>
             </div>
-        </div>
-        <div className="nav_tayfast">
-               <div className="icon">
-                   <i className="fa fa-shopping-cart" aria-hidden='true' onClick={showHideCart}/>
-                   {cartItems.length > 0 && (
+            <div className="nav_amass">
+                <div className="input_wrapper" >
+                    <input placeholder="search..." type="text"></input>
+                    <i className="fas fa-search" />
+                </div>
+            </div>
+            <div className="nav_tayfast">
+                <div className="icon">
+                    <i className="fa fa-shopping-cart" aria-hidden='true' onClick={showHideCart} />
+                    {cartItems.length > 0 && (
                         <div className='item_count'>
                             <span>{cartItems.length}</span>
                         </div>)}
-          </div>
-        </div>
-        </nav> 
+                </div>
+            </div>
+        </nav>
     );
 }
 
